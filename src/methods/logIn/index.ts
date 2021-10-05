ae1c7796a6984a1aa19c50b99bc48fbfdb6e59a8
Victor <ivchenko.victor.04@gmail.com>
2021-10-05 19:00:51 +0300

    плюс быстрый входа (без контекста)

--- src/methods/logIn/index.ts
+++ src/methods/logIn/index.ts
@@ -1,13 +1,27 @@
 import NS from "@NS";
+import Session from "@classes/Session";
 
 import signIn from "./methods/signIn";
 import openSession from "./methods/openSession";
+import expiredSession from "@methods/logOut/methods/expiredSession";
 
-export default function (this: NS) {
-  const { Client: client, credentials } = this;
+export default async function (this: NS) {
+  const { Client: client, session, credentials } = this;
   const { login, password, schoolName: school } = credentials;
 
+  if ((await expiredSession(client, session)) == false)
+    return session as Session;
+
   return signIn(login, password, school, client)
-    .then(openSession.bind(null, client))
+    .then((data) =>
+      !session
+        ? openSession(client, data)
+        : new Session({
+            ...session,
+            accessToken: data.at,
+            expiryDate: Date.now() + data.timeOut,
+            ver: data.ver,
+          })
+    )
     .then((session) => (this.session = session));
 }
