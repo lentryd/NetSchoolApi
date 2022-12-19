274a5f5978079f65b39d3ad145139fc3aed40ea6
Victor <ivchenko.victor.04@gmail.com>
2022-12-19 21:04:24 +0300

    Поддержка версии 5.13

--- src/methods/logIn/methods/schoolInfo.ts
+++ src/methods/logIn/methods/schoolInfo.ts
@@ -1,15 +1,67 @@
 import Client from "@classes/Client";
 
-interface SchoolAddress {
+interface SchoolSearchData {
   id: number;
+  inn: string;
+  ogrn: string;
   name: string;
   cityId: number;
-  stateId: number;
-  funcType: number;
-  countryId: number;
-  provinceId?: number;
-  parentCityId?: number;
-  municipalityDistrictId: number;
+  address: null;
+  shortName: string;
+  provinceId: number;
+}
+
+declare module SchoolPreLoginData {
+
+    export interface Country {
+        id: number;
+        name: string;
+    }
+
+    export interface State {
+        id: number;
+        name: string;
+    }
+
+    export interface Province {
+        kladr: string;
+        id: number;
+        name: string;
+    }
+
+    export interface City {
+        atoTypeName: string;
+        id: number;
+        name: string;
+    }
+
+    export interface Func {
+        id: number;
+        name: string;
+    }
+
+    export interface School {
+        id: number;
+        name: string;
+    }
+
+    export interface RootObject {
+        countries: Country[];
+        cid: number;
+        states: State[];
+        sid: number;
+        provinces: Province[];
+        pid: number;
+        cities: City[];
+        cn: number;
+        funcs: Func[];
+        sft: number;
+        schools: School[];
+        scid: number;
+        hlevels?: any;
+        ems?: any;
+    }
+
 }
 
 export interface SchoolInfo {
@@ -22,24 +74,25 @@ export interface SchoolInfo {
 }
 
 export default async function (client: Client, school: string | number) {
-  const schools: SchoolAddress[] = await client
-    .get("addresses/schools")
-    .then((res) => res.json() as any);
-
-  if (!schools || !schools.length)
-    throw new Error("Сетевой не вернул список школ");
+  const data = await Promise.all([
+    client.get("schools/search").then(res => res.json() as Promise<SchoolSearchData[]>),
+    client.get("prepareloginform").then(res => res.json() as Promise<SchoolPreLoginData.RootObject>),
+  ]).catch((e) => {
+    console.error(e);
+    throw new Error("Не удалось получить список школ");
+  });
 
   if (typeof school === "string") school = school.toLowerCase();
-  const result = schools.find(({ id, name }) =>
+  const result = data[0].find(({ id, name }) =>
     typeof school !== "string" ? id == school : name.toLowerCase() == school
   );
 
   if (!result) throw new Error("Не удалось найти школу");
 
   return {
-    cid: result.countryId,
-    sid: result.stateId,
-    pid: result.municipalityDistrictId,
+    cid: data[1].cid,
+    sid: data[1].sid,
+    pid: result.provinceId,
     cn: result.cityId,
     sft: 2,
     scid: result.id,
