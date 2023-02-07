b7f3348fb35786bf6ce87b5b9ccaada5764b19cf
Victor <ivchenko.victor.04@gmail.com>
2023-02-07 19:16:55 +0300

    исправлен поиск школы

--- src/methods/logIn/methods/schoolInfo.ts
+++ src/methods/logIn/methods/schoolInfo.ts
@@ -12,56 +12,54 @@ interface SchoolSearchData {
 }
 
 declare module SchoolPreLoginData {
+  export interface Country {
+    id: number;
+    name: string;
+  }
 
-    export interface Country {
-        id: number;
-        name: string;
-    }
+  export interface State {
+    id: number;
+    name: string;
+  }
 
-    export interface State {
-        id: number;
-        name: string;
-    }
+  export interface Province {
+    kladr: string;
+    id: number;
+    name: string;
+  }
 
-    export interface Province {
-        kladr: string;
-        id: number;
-        name: string;
-    }
+  export interface City {
+    atoTypeName: string;
+    id: number;
+    name: string;
+  }
 
-    export interface City {
-        atoTypeName: string;
-        id: number;
-        name: string;
-    }
+  export interface Func {
+    id: number;
+    name: string;
+  }
 
-    export interface Func {
-        id: number;
-        name: string;
-    }
+  export interface School {
+    id: number;
+    name: string;
+  }
 
-    export interface School {
-        id: number;
-        name: string;
-    }
-
-    export interface RootObject {
-        countries: Country[];
-        cid: number;
-        states: State[];
-        sid: number;
-        provinces: Province[];
-        pid: number;
-        cities: City[];
-        cn: number;
-        funcs: Func[];
-        sft: number;
-        schools: School[];
-        scid: number;
-        hlevels?: any;
-        ems?: any;
-    }
-
+  export interface RootObject {
+    countries: Country[];
+    cid: number;
+    states: State[];
+    sid: number;
+    provinces: Province[];
+    pid: number;
+    cities: City[];
+    cn: number;
+    funcs: Func[];
+    sft: number;
+    schools: School[];
+    scid: number;
+    hlevels?: any;
+    ems?: any;
+  }
 }
 
 export interface SchoolInfo {
@@ -75,19 +73,32 @@ export interface SchoolInfo {
 
 export default async function (client: Client, school: string | number) {
   const data = await Promise.all([
-    client.get("schools/search").then(res => res.json() as Promise<SchoolSearchData[]>),
-    client.get("prepareloginform").then(res => res.json() as Promise<SchoolPreLoginData.RootObject>),
+    client
+      .get("schools/search")
+      .then((res) => res.json() as Promise<SchoolSearchData[]>),
+    client
+      .get("prepareloginform")
+      .then((res) => res.json() as Promise<SchoolPreLoginData.RootObject>),
   ]).catch((e) => {
     console.error(e);
     throw new Error("Не удалось получить список школ");
   });
 
-  if (typeof school === "string") school = school.toLowerCase();
-  const result = data[0].find(({ id, name }) =>
-    typeof school !== "string" ? id == school : name.toLowerCase() == school
+  if (typeof school === "string") school = school.toLowerCase().trim();
+  const result = data[0].find(({ id, name, shortName }) =>
+    typeof school !== "string"
+      ? id == school
+      : name.toLowerCase().trim() == school ||
+        shortName.toLowerCase().trim() == school
   );
 
   if (!result) throw new Error("Не удалось найти школу");
+  if (typeof school === "string") {
+    console.info(
+      "\x1b[46m\x1b[30m INFO \x1b[0m",
+      `ID школы: ${result.id}. Советуем в будущем использовать ID школы вместо названия.`
+    );
+  }
 
   return {
     cid: data[1].cid,
