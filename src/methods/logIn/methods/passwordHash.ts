71f24236799507ae58b02c592f7848191b206d04
Victor <ivchenko.victor.04@gmail.com>
2024-12-03 21:14:03 +0300

    Обновлены зависимости @types/node и typescript до последних версий; исправлено кодирование строки в функции md5

--- src/methods/logIn/methods/passwordHash.ts
+++ src/methods/logIn/methods/passwordHash.ts
@@ -2,7 +2,7 @@ import { createHash } from "crypto";
 import { PasswordType } from "@NS";
 
 function md5(str: string): string {
-  return createHash("md5").update(str).digest("hex");
+  return createHash("md5").update(str, "utf8").digest("hex");
 }
 
 export default function (salt: string, password: PasswordType) {
